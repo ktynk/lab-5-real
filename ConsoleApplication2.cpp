f24989a3443ca873d12153961e7a3d283df2d105
ktynk <ktyn26k@gmail.com>
2025-11-12 21:39:19 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[3][2];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int z = 0; z < 2; z++)
+        {
+            cout << "wedite element pod indeksami: " << i << " " << z << endl;
+            cin >> arr[i][z];
+        }
+    }
+
+    int count = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        bool polozitelni = false;
+        for (int z = 0; z < 2; z++)
+        {
+            if (arr[i][z] > 0)
+            {
+                polozitelni = true;
+            }
+        }
+        if (polozitelni)
+        {
+            count++;
+            cout << "Stroca " << i << " polozitelna: ";
+            for (int z = 0; z < 2; z++)
+            {
+                cout << arr[i][z] << " ";
+            }
+            cout << endl;
+            if (i > 0)
+            {
+                cout << "Menyaem znaki v stroke " << i - 1 << ": ";
+                for (int z = 0; z < 2; z++)
+                {
+                    arr[i - 1][z] = -arr[i - 1][z];
+                    cout << arr[i - 1][z] << " ";
+                }
+                cout << endl;
+            }
+        }
+    }
+    cout << "Kolichestvo stroc polozitelnih: " << count << endl;
+}
